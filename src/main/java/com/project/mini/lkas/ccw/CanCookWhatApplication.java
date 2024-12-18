83015f349fca447544fcb03c969ab08d76a3b6f5
lkas96 <kwek_lawson@hotmail.com>
2024-12-18 18:47:31 +0800

    Springboot Starter Template + Jedis + Jakarta

---
+++ src/main/java/com/project/mini/lkas/ccw/CanCookWhatApplication.java
@@ -0,0 +1,13 @@
+package com.project.mini.lkas.ccw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanCookWhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanCookWhatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mini/lkas/ccw/CanCookWhatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mini.lkas.ccw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanCookWhatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
